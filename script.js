ea78144c86f0daca89c3a3e91cf897ef7824cefd
Redaska <reda.essakali@gmail.com>
2023-05-01 19:08:43 +0100

    Make a function that changes divs colors

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let container = document.querySelector(".container");
+let children;
 let child;
-for (let i = 0; i < 256; i++) {
-  child = document.createElement("div");
-  child.classList.add("child");
-  container.appendChild(child);
+function createDiv() {
+  for (let i = 0; i < 256; i++) {
+    child = document.createElement("div");
+    child.classList.add("child");
+    container.appendChild(child);
+    children = document.querySelectorAll(".child");
+  }
 }
+function changeColor() {
+  for (let i = 0; i < children.length; i++) {
+    const element = children[i];
+    element.addEventListener("mouseover", function () {
+      element.classList.add("hover");
+    });
+  }
+}
+createDiv();
+changeColor();
